b0432744c8beee726f78e0f166b4e17b3ca82da5
PlusF <rkaneda896@gmail.com>
2024-02-02 13:30:03 +0900

    use pathlib

--- dataloader/HDFReader.py
+++ dataloader/HDFReader.py
@@ -1,14 +1,13 @@
-import os.path
-
+from pathlib import Path
 import h5py
 
 
 class HDFReader:
-    def __init__(self, path):
-        if os.path.splitext(path)[1] != '.hdf5':
+    def __init__(self, p: Path):
+        if p.suffix != '.hdf5':
             raise ValueError('File extension must be .hdf5')
-        self.path = path
-        self.file = h5py.File(path, 'r')
+        self.path = p
+        self.file = h5py.File(p, 'r')
 
     def __getitem__(self, item):
         return self.file[item]
@@ -18,11 +17,11 @@ class HDFReader:
 
 
 class HDFWriter:
-    def __init__(self, path):
-        if os.path.splitext(path)[1] != '.hdf5':
+    def __init__(self, p: Path):
+        if p.suffix != '.hdf5':
             raise ValueError('File extension must be .hdf5')
-        self.path = path
-        self.file = h5py.File(path, 'w')
+        self.path = p
+        self.file = h5py.File(p, 'w')
 
     def __setitem__(self, key, value):
         self.file[key] = value
@@ -32,8 +31,8 @@ class HDFWriter:
 
 
 class RamanHDFReader(HDFReader):
-    def __init__(self, path):
-        super().__init__(path)
+    def __init__(self, p: Path):
+        super().__init__(p)
         self.xdata = self.file['xdata'][:]
         self.spectra = self.file['spectra'][:]
 
@@ -53,8 +52,8 @@ class RamanHDFReader(HDFReader):
 
 
 class RamanHDFWriter(HDFWriter):
-    def __init__(self, path):
-        super().__init__(path)
+    def __init__(self, p: Path):
+        super().__init__(p)
 
     def create_attr(self, key, value):
         self.file.attrs[key] = value
